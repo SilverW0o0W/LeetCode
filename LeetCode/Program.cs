b2097d0849a1972641ba5161cb7ec940658cee4a
Silver <silver.codingcat@gmail.com>
2017-07-13 14:13:56 +0800

    Reverse Integer

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Two_Sum;
+
+
+//using Two_Sum;
+using Reverse_Integer;
 
 namespace LeetCode
 {
@@ -12,6 +15,7 @@ namespace LeetCode
         static void Main(string[] args)
         {
             Solution solution = new Solution();
+            int a = solution.Reverse(-2147483648);
             Console.WriteLine();
         }
     }
---
+++ Reverse Integer/Reverse Integer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Integer
+{
+    public class Solution
+    {
+        public int Reverse_Self(int x)
+        {
+            if (x == 0 || x > int.MaxValue || x <= int.MinValue) return 0;
+            Queue<int> queue = new Queue<int>();
+            bool endWithZero = true;
+            bool flag = x > 0;
+            if (!flag) x = Math.Abs(x);
+            while (x != 0)
+            {
+                int temp = x % 10;
+                endWithZero = temp == 0 && endWithZero;
+                if (!endWithZero) queue.Enqueue(temp);
+                x /= 10;
+            }
+            string value = string.Empty;
+            while (queue.Count > 0) value += queue.Dequeue();
+            long result = long.Parse(value);
+            result = (result > (flag ? int.MaxValue : Math.Abs((long)int.MinValue))) ? 0 : result;
+            return (int)(flag ? result : -result);
+        }
+
+        public int Reverse(int x)
+        {
+            var rev = 0;
+            while (x != 0)
+            {
+                try
+                {
+                    rev = checked(rev * 10 + x % 10);
+                    x /= 10;
+                }
+                catch
+                {
+                    rev = 0;
+                    break;
+                }
+            }
+            return rev;
+        }
+    }
+}
