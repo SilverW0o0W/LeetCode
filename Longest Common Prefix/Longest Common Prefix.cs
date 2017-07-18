7b6358c60b701e1d5767e74c17ec6010ceaf03db
Silver <silver.codingcat@gmail.com>
2017-07-18 23:14:05 +0800

    Add description

--- Longest Common Prefix/Longest Common Prefix.cs
+++ Longest Common Prefix/Longest Common Prefix.cs
@@ -8,6 +8,10 @@ namespace Longest_Common_Prefix
 {
     public class Solution
     {
+        //https://leetcode.com/problems/longest-common-prefix/
+        /*
+         * Write a function to find the longest common prefix string amongst an array of strings.
+         */
         public string LongestCommonPrefix(string[] strs)
         {
             for (int i = 0; i < strs[0].Length; i++)
