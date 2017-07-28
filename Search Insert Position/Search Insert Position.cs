992b5553f908ae2b67f6ebb29500f3180ce68411
Silver <silver.codingcat@gmail.com>
2017-07-28 16:51:45 +0800

    Search Insert Position(Clear)

--- Search Insert Position/Search Insert Position.cs
+++ Search Insert Position/Search Insert Position.cs
@@ -7,20 +7,30 @@ using System.Threading.Tasks;
 namespace Search_Insert_Position
 {
     //https://leetcode.com/problems/search-insert-position
+    /*
+     * Given a sorted array and a target value, return the index if the target is found.If not, return the index where it would be if it were inserted in order.
+     * You may assume no duplicates in the array.
+     * Here are few examples.
+     * [1, 3, 5, 6], 5 → 2
+     * [1,3,5,6], 2 → 1
+     * [1,3,5,6], 7 → 4
+     * [1,3,5,6], 0 → 0
+     */
+
     public class Solution
     {
-        public int SearchInsert(int[] nums, int target)
+        public int SearchInsert_Self(int[] nums, int target)
         {
             if (nums[0] >= target) return 0;
-            if (nums[nums.Length - 1] <= target) return nums.Length - 1;
+            if (nums[nums.Length - 1] < target) return nums.Length;
             return BinarySearch(nums, target, 0, nums.Length - 1);
         }
 
         private int BinarySearch(int[] nums, int target, int pre, int post)
         {
-            if (pre == post)
+            if (pre >= post - 1)
             {
-                return pre;
+                return post;
             }
             int mid = (pre + post) / 2;
             if (nums[mid] == target)
@@ -37,15 +47,27 @@ namespace Search_Insert_Position
                 {
                     pre = mid;
                 }
-                BinarySearch(nums, target, pre, post);
-                return mid;
+                return BinarySearch(nums, target, pre, post);
             }
+        }
+
+        public int SearchInsert(int[] A, int target)
+        {
+            int low = 0, high = A.Length - 1;
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (A[mid] == target) return mid;
+                else if (A[mid] > target) high = mid - 1;
+                else low = mid + 1;
+            }
+            return low;
         }
 
         public void Run()
         {
             int[] nums = new int[] { 1, 3, 5 };
-            int result = SearchInsert(nums, 2);
+            int result = SearchInsert_Self(nums, 2);
         }
     }
 }
