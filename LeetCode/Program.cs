466659a242676d37ee3f925ec867de4a035cde1b
Silver <silver.codingcat@gmail.com>
2017-08-07 13:58:46 +0800

    ZigZag Conversion

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -18,7 +18,8 @@ using System.Threading.Tasks;
 //using LeetCode.String_to_Integer;
 //using LeetCode.Longest_Substring_Without_Repeating_Characters;
 //using LeetCode.Median_of_Two_Sorted_Arrays;
-using LeetCode.Longest_Palindromic_Substring;
+//using LeetCode.Longest_Palindromic_Substring;
+using LeetCode.ZigZag_Conversion;
 
 namespace LeetCode
 {
---
+++ LeetCode/Solutions/ZigZag Conversion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.ZigZag_Conversion
+{
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1) return s;
+            int n = numRows - 1;
+            int loopNum = 2 * n;
+            int maxLine = (s.Length - 1) / loopNum;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j <= maxLine; j++)
+                {
+                    int index = loopNum * j + i;
+                    if (index < s.Length)
+                    {
+                        sb.Append(s[index]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    if (i == 0 || i == n)
+                    {
+                        continue;
+                    }
+                    index = loopNum * (j + 1) - i;
+                    if (index < s.Length)
+                    {
+                        sb.Append(s[index]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        public void Run()
+        {
+            string s = "PAYPALISHIRING";
+            int numRows = 3;
+            string result = Convert(s, numRows);
+        }
+    }
+}
