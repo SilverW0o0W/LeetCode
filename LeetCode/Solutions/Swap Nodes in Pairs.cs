be4f9ca25b2aa455e8af63c129469e0d6790ba04
Silver <silver.codingcat@gmail.com>
2017-10-06 23:00:15 +0800

    Add Swap Nodes in Pairs

---
+++ LeetCode/Solutions/Swap Nodes in Pairs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Swap_Nodes_in_Pairs
+{
+    //https://leetcode.com/problems/swap-nodes-in-pairs/
+    /*
+     * Given a linked list, swap every two adjacent nodes and return its head.
+     * For example,
+     * Given 1->2->3->4, you should return the list as 2->1->4->3.
+     * Your algorithm should use only constant space.You may not modify the values in the list, only nodes itself can be changed.
+     */
+
+    // Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public class Solution
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            return null;
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}
