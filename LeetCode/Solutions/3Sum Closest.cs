c0d6974c88d3a265fac43f721f1d55d2f996ba6c
Silver <silver.codingcat@gmail.com>
2017-12-18 18:11:40 +0800

    3Sum Closest

--- LeetCode/Solutions/3Sum Closest.cs
+++ LeetCode/Solutions/3Sum Closest.cs
@@ -8,41 +8,37 @@ namespace LeetCode.ThreeSum_Closest
 {
     public class Solution
     {
+        //https://leetcode.com/problems/3sum-closest/
+        /*
+         * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.Return the sum of the three integers.You may assume that each input would have exactly one solution.
+         * For example, given array S = { -1 2 1 - 4 }, and target = 1.
+         * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+        */
         public int ThreeSumClosest(int[] nums, int target)
         {
             Array.Sort(nums);
-            long diff = target > 0 ? (long)target - int.MinValue : (long)-target + int.MaxValue;
-            int result = int.MinValue;
+            int result = nums[0] + nums[1] + nums[nums.Length - 1];
             for (int i = 0; i < nums.Length - 2; i++)
             {
-                if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+                int start = i + 1, end = nums.Length - 1;
+                while (start < end)
                 {
-                    int low = i + 1;
-                    int high = nums.Length - 1;
-                    while (low < high)
+                    int sum = nums[i] + nums[start] + nums[end];
+                    if (sum == target)
                     {
-                        int sum = nums[i] + nums[low] + nums[high];
-                        if (sum == target)
-                        {
-                            return target;
-                        }
-                        long tempDiff = target > 0 ? (long)target - sum : (long)-target - sum;
-                        if (Math.Abs(tempDiff) > diff)
-                        {
-                            break;
-                        }
-                        diff = target - sum;
+                        return target;
+                    }
+                    else if (sum > target)
+                    {
+                        end--;
+                    }
+                    else
+                    {
+                        start++;
+                    }
+                    if (Math.Abs(sum - target) < Math.Abs(result - target))
+                    {
                         result = sum;
-                        if (target >= 0)
-                        {
-                            if (target - sum > 0) low++;
-                            else high--;
-                        }
-                        else
-                        {
-                            if (target + sum < 0) low++;
-                            else high--;
-                        }
                     }
                 }
             }
